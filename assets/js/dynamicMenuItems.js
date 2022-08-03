6a6d4433f2ff1c9b2a292eca44ef5b77bb90b39b
Elpiu <49283847+Elpiu@users.noreply.github.com>
2022-08-04 01:22:36 +0200

    base per e-book
    piccolo website a componenti con sezioni aggiunte automaticamente alla navbar

---
+++ assets/js/dynamicMenuItems.js
@@ -0,0 +1,21 @@
+/**
+ * Costruisce dinamicamente i  titoli dei
+ * paragrafi e li inserisce in un menu, il div deve avere
+ * content="qualcosa" e un nome name="nome da mostrare nel menu".
+ *
+ * @param {String} content name of the attribute where search
+ * @param {String} name name to display after into menu
+ */
+function load_and_set(content, name) {
+  var contents = $("div[" + content + "]");
+  for (i = 0; i < contents.length; i++) {
+    var str =
+      '<li class="nav-item">' +
+      '<a class="nav-link active" href="#' +
+      contents[i].id +
+      '">' +
+      contents[i].getAttribute(name) +
+      "</a> </li>";
+    $("#menu").append(str);
+  }
+}
